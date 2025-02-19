d76819068525b6e14987e85a4d5ac7fe135623a2
Judah Lomo <judahllomo@gmail.com>
2025-02-18 18:48:01 -0600

    Initial Commit

---
+++ TriviaBlasters/TriviaBlastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaBlastersApp.swift
+//  TriviaBlasters
+//
+//  Created by Judah Lomo on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TriviaBlastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TriviaBlastersTests/TriviaBlastersTests.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaBlastersTests.swift
+//  TriviaBlastersTests
+//
+//  Created by Judah Lomo on 2/18/25.
+//
+
+import Testing
+@testable import TriviaBlasters
+
+struct TriviaBlastersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
